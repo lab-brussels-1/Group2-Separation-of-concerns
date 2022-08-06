ffa420d1aac346fb6114c493b7876ee9fa987fe2
minju <minjupgeorge@gmail.com>
2022-08-06 23:02:55 +0200

    code style issues solved

--- client/src/init.js
+++ client/src/init.js
@@ -1,13 +1,13 @@
-    document.getElementById('add-product').addEventListener('click', (event) =>{
-	// read & process user input
-	const form = event.target.form;
-	const valueToadd = form.value.value;
-	let newvariable;
-	newvariable = String(valueToadd);
-	var x = document.getElementById('mySelect');
-	var option = document.createElement('option');
-	option.text = newvariable;
-	x.add(option, x[0]);
-	// communicate result to user
-	// document.getElementById('added-value').innerHTML =newvariable;
+document.getElementById('add-product').addEventListener('click', (event) => {
+// read & process user input
+const form = event.target.form;
+const valueToadd = form.value.value;
+let newvariable;
+newvariable = String(valueToadd);
+var x = document.getElementById('mySelect');
+var option = document.createElement('option');
+option.text = newvariable;
+x.add(option, x[0]);
+// communicate result to user
+// document.getElementById('added-value').innerHTML =newvariable;
 });
