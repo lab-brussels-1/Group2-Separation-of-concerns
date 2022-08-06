39edd480b0171f6801f8deb600df7a7ae66ac513
minju <minjupgeorge@gmail.com>
2022-08-07 00:12:32 +0200

    format run for init

--- client/src/init.js
+++ client/src/init.js
@@ -1,9 +1,9 @@
 document.getElementById('add-product').addEventListener('click', (event) => {
-  const form = event.target.form;
-  const valueToadd = form.value.value;
-  const newvariable = String(valueToadd);
-  let x = document.getElementById('mySelect');
-  let option = document.createElement('option');
-  option.text = newvariable;
-  x.add(option, x[0]);
+	const form = event.target.form;
+	const valueToadd = form.value.value;
+	const newvariable = String(valueToadd);
+	let x = document.getElementById('mySelect');
+	let option = document.createElement('option');
+	option.text = newvariable;
+	x.add(option, x[0]);
 });
