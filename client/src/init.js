87a9f52bc541444d66c14e79809bcc9d1e3bfe14
minju <minjupgeorge@gmail.com>
2022-08-06 23:23:05 +0200

    format run

--- client/src/init.js
+++ client/src/init.js
@@ -1,12 +1,8 @@
 document.getElementById('add-product').addEventListener('click', (event) => {
-	// read & process user input
-
 	const form = event.target.form;
 	const valueToadd = form.value.value;
-
 	let newvariable;
 	newvariable = String(valueToadd);
-
 	var x = document.getElementById('mySelect');
 	var option = document.createElement('option');
 	option.text = newvariable;
