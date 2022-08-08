73114cb95d44a1cc8806ab3fdd6d240e34b5d1dc
minju <minjupgeorge@gmail.com>
2022-08-08 18:24:10 +0200

    html corrected

--- client/src/init.js
+++ client/src/init.js
@@ -1,6 +1,6 @@
 document.getElementById('add-product').addEventListener('click', (event) => {
   const item = event.target.form;
-  const additem= item.value.value;
+  const additem = item.value.value;
   const x = document.getElementById('mySelect');
   const option = document.createElement('option');
   option.text = additem;
