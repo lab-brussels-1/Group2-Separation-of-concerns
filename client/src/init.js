5e77b9781bb7f49795b816f879a742047d417b10
minju <minjupgeorge@gmail.com>
2022-08-08 18:03:31 +0200

    form removed from html

--- client/src/init.js
+++ client/src/init.js
@@ -1,9 +1,9 @@
 document.getElementById('add-product').addEventListener('click', (event) => {
-  const item = event.target.form;
-  const valueToadd = item.value.value;
-  const newvariable = String(valueToadd);
+  const item = document.getElementById('input-text-id').value
+ // const valueToadd = item.value.value;
+ // const newvariable = String(valueToadd);
   const x = document.getElementById('mySelect');
   const option = document.createElement('option');
-  option.text = newvariable;
+  option.text = item;
   x.add(option, x[0]);
 });
