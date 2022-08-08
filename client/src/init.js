fd7151994955a03a7316c604160d801a181f98b6
minju <minjupgeorge@gmail.com>
2022-08-08 16:54:20 +0200

    tab space removed from prettierrcfile

--- client/src/init.js
+++ client/src/init.js
@@ -1,9 +1,9 @@
 document.getElementById('add-product').addEventListener('click', (event) => {
-	const form = event.target.form;
-	const valueToadd = form.value.value;
-	const newvariable = String(valueToadd);
-	const x = document.getElementById('mySelect');
-	const option = document.createElement('option');
-	option.text = newvariable;
-	x.add(option, x[0]);
+  const item = event.target.form;
+  const valueToadd = item.value.value;
+  const newvariable = String(valueToadd);
+  const x = document.getElementById('mySelect');
+  const option = document.createElement('option');
+  option.text = newvariable;
+  x.add(option, x[0]);
 });
